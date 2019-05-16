d7c96103c8d42a09280335b5217329992e4c7b05
Dmitry Shmidt <dimitrysh@google.com>
2019-05-15 19:17:21 -0700

    rtk_btusb: Remove use of PM usage counter
    It is taken away in stable update
    
    Change-Id: Ia87f80c67d4cf8af2d47549a0c476cde1a465da4
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/bluetooth/rtk_btusb.c
+++ drivers/bluetooth/rtk_btusb.c
@@ -3057,8 +3057,7 @@ static int btusb_open(struct hci_dev *hdev)
     struct btusb_data *data = GET_DRV_DATA(hdev);
     int err = 0;
 
-    RTKBT_INFO("%s: Start, PM usage count %d", __func__,
-            atomic_read(&(data->intf->pm_usage_cnt)));
+    RTKBT_INFO("%s: Start", __func__);
 
     err = usb_autopm_get_interface(data->intf);
     if (err < 0)
@@ -3088,16 +3087,14 @@ static int btusb_open(struct hci_dev *hdev)
 
 done:
     usb_autopm_put_interface(data->intf);
-    RTKBT_INFO("%s: End, PM usage count %d", __func__,
-            atomic_read(&(data->intf->pm_usage_cnt)));
+    RTKBT_INFO("%s: End", __func__);
     return 0;
 
 failed:
     clear_bit(BTUSB_INTR_RUNNING, &data->flags);
     clear_bit(HCI_RUNNING, &hdev->flags);
     usb_autopm_put_interface(data->intf);
-    RTKBT_ERR("%s: Failed, PM usage count %d", __func__,
-            atomic_read(&(data->intf->pm_usage_cnt)));
+    RTKBT_ERR("%s: Failed", __func__);
     return err;
 }
 
@@ -3603,8 +3600,7 @@ static void btusb_waker(struct work_struct *work)
     struct btusb_data *data = container_of(work, struct btusb_data, waker);
     int err;
 
-    RTKBT_DBG("%s: PM usage count %d", __func__,
-            atomic_read(&data->intf->pm_usage_cnt));
+    RTKBT_DBG("%s: Wake", __func__);
 
     err = usb_autopm_get_interface(data->intf);
     if (err < 0)
